36d09013a13557a9e553741607e93803c1d022fb
Diego Dominguez <34127316+diegodf8@users.noreply.github.com>
2018-03-09 11:59:34 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    
+	    String rutaFichero ="Texto.txt";
+	    FileReader fr;
+        BufferedReader br;
+        double Nota = 0.0;
+        int NumNotas = 0;
+        String [] linea;
+        
+        try {
+            fr = new FileReader(rutaFichero);
+            br = new BufferedReader(fr);
+            String cadena = null;
+            while ((cadena = br.readLine()) != null){
+                System.out.println(cadena);
+                linea = cadena.split("\\|");
+                System.out.println("*****************");
+                for(String aux: linea){
+                    System.out.println("-" + aux + "-");
+                    Nota+=Double.parseDouble(aux);
+                    NumNotas++;
+                }
+            }
+            System.out.println(Nota/NumNotas + "---> Nota Media");
+            br.close();
+            fr.close();
+        }catch (FileNotFoundException ex){
+            ex.printStackTrace();
+        }catch (IOException ex2){
+            ex2.printStackTrace();
+        }
+	    
+	    
+    }
+}
